04cf043efe3819d8074a6b0f29ae1d8ee3b90b2d
platonzhuman <platonzhuman@bk.ru>
2026-04-22 23:09:39 +0800

    create project and start coding colors

---
+++ terminal_color/src/colors.py
@@ -0,0 +1,31 @@
+COLORS = {
+    "red": "\033[31m",
+    "green": "\033[32m",
+    "blue": "\033[34m",
+    "yellow": "\033[33m",
+    "cyan": "\033[36m",
+    "magenta": "\033[35m",
+    "white": "\033[37m",
+    "black": "\033[30m",
+    "bg_red": "\033[41m",
+    "bg_green": "\033[42m",
+    "bg_blue": "\033[44m",
+    "bg_yellow": "\033[43m",
+    "bg_cyan": "\033[46m",
+    "bg_magenta": "\033[45m",
+    "bg_white": "\033[47m",
+    "bg_black": "\033[40m",
+    "bold": "\033[1m",
+    "underline": "\033[4m",
+    "reset": "\033[0m"
+}
+
+def setcolor(color):
+    if color not in COLORS:
+        print(f"Ошибка: цвет '{color}' не найден")
+        return
+    print(COLORS[color], end="")
+
+def reset():
+    print(COLORS["reset"], end="")
+
