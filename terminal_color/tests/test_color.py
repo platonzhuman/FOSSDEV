f68190628b3ad0c40f1f668890e7e5068b24c989
platonzhuman <platonzhuman@bk.ru>
2026-04-22 23:31:53 +0800

    create basic test py file for test my project

---
+++ terminal_color/tests/test_color.py
@@ -0,0 +1,13 @@
+from src.colors import COLORS, setcolor, reset
+
+def test_color():
+    assert "red" in COLORS
+    assert "bg_blue" in COLORS
+    assert "bold" in COLORS
+    assert "reset" in COLORS
+
+def test_reset():
+    assert COLORS["reset"] == "\003[0m"
+
+def test_bad():
+    setcolor("badcolor")
