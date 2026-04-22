492cd238314e4eea2b518d0b76977dacdf161487
platonzhuman <platonzhuman@bk.ru>
2026-04-22 23:32:34 +0800

    create setup.py for inctruction in PYPI

---
+++ terminal_color/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="terminal-color-tool",
+    version="0.1.0",
+    author="NoName ^_^",
+    author_email="77pl77@inbox.ru",
+    description="Change colors in your terminal ;",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/platonzhuman/FOSSDEV/tree/feature/terminal_color/terminal_color",
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    python_requires=">=3.10",
+    entry_points={
+        "console_scripts": [
+            "terminal-color=terminal_color.cli:main",
+        ],
+    },
+)
