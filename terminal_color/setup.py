a5c821f23abc26bc877368e57334b3bebfc52357
platonzhuman <platonzhuman@bk.ru>
2026-04-23 22:46:59 +0800

    project ready ^|_|^

--- terminal_color/setup.py
+++ terminal_color/setup.py
@@ -4,8 +4,8 @@ with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
 setup(
-    name="terminal-color-tool",
-    version="0.1.0",
+    name="terminal_color_tool",
+    version="0.1.1",
     author="NoName ^_^",
     author_email="77pl77@inbox.ru",
     description="Change colors in your terminal",
